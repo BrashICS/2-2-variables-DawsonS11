20394141195920a3452c97b8b9e7526c9d3dc53e
Dawson <dawson.sheehy@stu.ocsb.ca>
2024-10-07 10:55:18 -0400

    Im done the work lets go!!! :D

--- main.js
+++ main.js
@@ -13,3 +13,37 @@
 let it_is_christmas = false
 let teaching_partner = "Mr. Squirrel"
 let number_of_students = 27
+
+// READ ME TASK
+let age = 16
+
+console.log("I am " + age + " years old")
+
+// TASK
+
+let length = 10
+let width = 5
+let area = length * width
+
+console.log(area)
+
+console.log("Area: " + area)
+
+let first_name = "Santa"
+let last_name = "Claws"
+
+last_name = "Claus"
+
+console.log("i still believe in " + first_name + " " +  last_name)
+
+console.log(typeof length)
+console.log(typeof area)
+console.log(typeof last_name)
+
+const pi = 3.14
+
+let radius = 4
+
+let circle_area = pi * radius * radius
+
+console.log(circle_area)
